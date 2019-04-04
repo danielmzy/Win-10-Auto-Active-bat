fb874de204d028a0290658188fff4b15390bcb6b
Daniel MZY <47544636+danielmzy@users.noreply.github.com>
2019-04-04 09:48:14 -0400

    Add files via upload

---
+++ Win 10 Auto Active-Simple.bat
@@ -0,0 +1,22 @@
+@echo off
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if '%errorlevel%' NEQ '0' (
+goto UACPrompt
+) else ( goto gotAdmin )
+:UACPrompt
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+"%temp%\getadmin.vbs"
+exit /B
+:gotAdmin
+if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+pushd "%CD%"
+CD /D "%~dp0"
+
+start  "Windows 10 Auto Active" "C:\Windows\System32\cmd.exe" 
+
+slmgr /ato
+
+taskkill /f /im cmd.exe
+
+exit
